0ea759a8111a524de087ebc3df67a7f9bbb2080b
astronaut-nemo <astronautnemo@gmail.com>
2022-08-29 15:00:40 +0300

    Week 2:  Lesson 2.4 (ongoing)
    Reached step 4/6 of lesson 2.4

--- Week 2/Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Week 2/Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -5,27 +5,32 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     // Variables
-    public GameObject[] animalPrefabs;// Array for holding the animal game objects
-    private float spawnRangeX = 10.0f;// Range for horizontal posiiton of spawning
-    private float spawnPosZ = 20.0f;// Coordinate for spawning along z-axis
+    public GameObject[] animalPrefabs; // Array for holding the animal game objects
+    private float spawnRangeX = 10.0f; // Range for horizontal posiiton of spawning
+    private float spawnPosZ = 20.0f; // Coordinate for spawning along z-axis
+    private float startDelay = 2.0f; // Delay before spawning starts
+    private float spawnInterval = 1.5f; // Interval between spawning new animals
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval); // Calls the SpawnRandomAnimal(), 'startDelay' seconds after the game starts, every 'spawnInterval' seconds
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    // SpawnRandomAnimal is a method that spawns random animals (like the name says ( ˙▿˙ ) )
+    void SpawnRandomAnimal()
+    {
         // Local variables
         int animalIndex = Random.Range(0, animalPrefabs.Length); // Randomly select an index in the array
-        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ); // Randomise the horizontanl position within the range
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ); // Randomise the horizontal position within the range
 
-        // When the 'S' key is pressed, spawn an animal
-        if (Input.GetKeyDown(KeyCode.S)) 
-        {
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
-        }
+        // Clone a random animal from the array
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
