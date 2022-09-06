ee604f6688169e55491dd69482256e370b791e4b
astronaut-nemo <astronautnemo@gmail.com>
2022-09-06 15:26:16 +0300

    Week 3: Leeson 3.4 (ongoing)
    Reached step 5/6 of lesson 3.4

--- Week 3/Prototype 3/Assets/Scripts/PlayerController.cs
+++ Week 3/Prototype 3/Assets/Scripts/PlayerController.cs
@@ -8,11 +8,16 @@ public class PlayerController : MonoBehaviour
 {
     //Variables
     private Rigidbody playerRb; // Holds the Rigidbody of the player
+    private Animator playerAnim; // Holds the Animator of the player
+    public ParticleSystem explosionParticle; // Holds the explosions fx
+    public ParticleSystem dirtParticle; // Holds the dirt splatter fx
+
     public float jumpForce; // How strong is the jump/distance of the jump
     public float gravityModifier; // Allows us to change the value of gravity as we please
+    
     public bool isOnGround = true; // Check if player is on the ground
     public bool gameOver = false; // Check if player collides with obstacle thus the game is over
-    private Animator playerAnim;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -31,6 +36,7 @@ public class PlayerController : MonoBehaviour
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); // Applies an instant force to the game object
             isOnGround = false; // The player is not on the ground while jumping
             playerAnim.SetTrigger("Jump_trig"); // Sets the Jump_trig Animator parameter
+            dirtParticle.Stop(); // Don't play the dirt particle when in the air
         }
 
     }
@@ -39,16 +45,21 @@ public class PlayerController : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
-            // Whenever the player collides with "Ground" game object, in this case the ground, set isOnGround boolean to true
+            // Whenever the player collides with "Ground" game object, in this case the ground, set isOnGround boolean to true + play dirt particle
             isOnGround = true;
+            dirtParticle.Play();
         }
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
-            // Whenever the player collides with "Obstacle" game object, in this case the ground, set gameOver boolean to true
+            /* Whenever the player collides with "Obstacle" game object, in this case the ground, set gameOver boolean to true + play death animation
+                + play explosion particle + stop dirt particle
+            */
             gameOver = true;
             Debug.Log("Game Over!"); // Print Game Over message
             playerAnim.SetBool("Death_b", true); // Set the death animation
             playerAnim.SetInteger("DeathType_int", 1); // Set the death animation type to Death_01
+            explosionParticle.Play(); // Play the explosion particle system
+            dirtParticle.Stop(); // Don't play the dirt particle when dead
         }
         
     }
