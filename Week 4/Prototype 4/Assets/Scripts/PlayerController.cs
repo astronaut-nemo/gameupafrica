67e4797cfdc04ecd5a4359be1f5020826994877d
astronaut-nemo <astronautnemo@gmail.com>
2022-09-12 23:58:21 +0300

    Week 4: Lesson 4.3 (ongoing)
    Reached step 3/6  in lesson 4.3

--- Week 4/Prototype 4/Assets/Scripts/PlayerController.cs
+++ Week 4/Prototype 4/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     //Variables
     private float speed = 8.0f; // Speed of movement
+    public bool hasPowerUp = false; // TO check if player has collided with the powerup
 
     // References
     private Rigidbody playerRb; // Holds reference to gameobject Rigidbody
@@ -25,5 +26,15 @@ public class PlayerController : MonoBehaviour
         
         // Apply force to playerRb in the direction of the forward axis of "Focal Point" that is controlled by forwardInput at the speed speed
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+
+    // Checking for collision between player and powerup, and destorying the powerup
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PowerUp"))
+        {
+            hasPowerUp = true; // Set powerup bool to true
+            Destroy(other.gameObject); // Destroy the powerup
+        }
     }
 }
