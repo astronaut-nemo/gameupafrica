aac999a5182e1d4b361875efb54ba2bcc275476c
astronaut-nemo <astronautnemo@gmail.com>
2022-09-13 14:36:17 +0300

    Week 4: Lesson 4.3
    Completed lesson 4.3

--- Week 4/Prototype 4/Assets/Scripts/PlayerController.cs
+++ Week 4/Prototype 4/Assets/Scripts/PlayerController.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     //Variables
-    private float speed = 8.0f; // Speed of movement
-    public bool hasPowerUp = false; // TO check if player has collided with the powerup
+    private float speed = 5.0f; // Speed of movement
+    public bool hasPowerUp = false; // To check if player has collided with the powerup
+    private float powerUpStrength = 15.0f; // Strength of yeeting enemy on collision
 
     // References
     private Rigidbody playerRb; // Holds reference to gameobject Rigidbody
     private GameObject focalPoint; // Holds reference to the GameObject "Focal Point"
+    public GameObject powerupIndicator; // Holds powerup indicator
     
     // Start is called before the first frame update
     void Start()
@@ -26,6 +28,9 @@ public class PlayerController : MonoBehaviour
         
         // Apply force to playerRb in the direction of the forward axis of "Focal Point" that is controlled by forwardInput at the speed speed
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+
+        // Powerup indicator to follow player
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
     }
 
     // Checking for collision between player and powerup, and destorying the powerup
@@ -34,7 +39,34 @@ public class PlayerController : MonoBehaviour
         if (other.CompareTag("PowerUp"))
         {
             hasPowerUp = true; // Set powerup bool to true
-            Destroy(other.gameObject); // Destroy the powerup
+            powerupIndicator.gameObject.SetActive(true); // Activate the powerup indicator
+
+            Destroy(other.gameObject); // Destroy the powerup object
+            StartCoroutine(PowerUpCountdownRoutine()); // Start powerup countdown coroutine
+        }
+    }
+
+    // PowerUp Countdown Timer Corroutine
+    IEnumerator PowerUpCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7); // Wait 7 seconds after powerup is activated
+        hasPowerUp = false; // Deactivate the powerup
+        powerupIndicator.gameObject.SetActive(false); // Deactivate the powerup indicator
+    }
+
+
+    // Checking for collision between player and enemy, while the player has a powerup. If conditions are met, yeet enemy away from player.
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerUp)
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>(); // Get enemy's Rigidbody component and assign it to the var enemyRigidbody
+            Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position; // Get vector in the direction to move the enemy away from the player
+
+            // Move the eneny away from teh player with an Impulse force
+            enemyRigidbody.AddForce(awayFromPlayer *powerUpStrength, ForceMode.Impulse);
+            // Notification that player has  powerup and collided with the enemy
+            Debug.Log("Collided with: " + collision.gameObject.name + " with the powerup set to " + hasPowerUp);
         }
     }
 }
