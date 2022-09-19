598c50739a8168d54357a3abc18ee7dcb09fc006
astronaut-nemo <astronautnemo@gmail.com>
2022-09-19 14:52:40 +0300

    Week 4: Challenge 4
    Completed Challenge 4, though there is a Null Exception error for the reference to EnemyX.cs with enemySpeed.

---
+++ Week 4/Challenge 4/Assets/Challenge 4/Scripts/EnemyX.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyX : MonoBehaviour
+{
+    // Variables
+    public float speed;
+
+    // References
+    private Rigidbody enemyRb;
+    private GameObject playerGoal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        playerGoal = GameObject.Find("Player Goal"); // Assign the playerGoal variable to the Player Goal GameObject
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set enemy direction towards player goal and move there
+        Vector3 lookDirection = (playerGoal.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection * speed * Time.deltaTime);
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // If enemy collides with either goal, destroy it
+        if (other.gameObject.name == "Enemy Goal")
+        {
+            Destroy(gameObject);
+        } 
+        else if (other.gameObject.name == "Player Goal")
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+}
---
+++ Week 4/Challenge 4/Assets/Challenge 4/Scripts/PlayerControllerX.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    // Variables
+    private Rigidbody playerRb;
+    public float speed = 500;
+    private GameObject focalPoint;
+
+    public bool hasPowerup;
+    public GameObject powerupIndicator;
+    public int powerUpDuration = 5; // Holds the cooldown time for the powerup
+
+    private float normalStrength = 10; // how hard to hit enemy without powerup
+    private float powerupStrength = 25; // how hard to hit enemy with powerup
+
+    public ParticleSystem boostParticle; // Holds the boost particles
+    
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    void Update()
+    {
+        // Add force to player in direction of the focal point (and camera)
+        float verticalInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * verticalInput * speed * Time.deltaTime); 
+
+        // Set powerup indicator position to beneath player
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.6f, 0);
+
+        // Add boost to the player when powerup is active
+        PlayerBoost();
+
+    }
+
+
+
+    // Sets player boosting when the player has a powerup and presses spacebar
+    void PlayerBoost()
+    {
+        // Add boost to the player when powerup is active
+        if(Input.GetKeyDown(KeyCode.Space) && hasPowerup == true)
+        {
+            boostParticle.Play(); // Play the bostParticle effect
+            speed = 750; // Increase player speed
+        }
+        else if(Input.GetKeyUp(KeyCode.Space) || hasPowerup != true) //  && hasPowerup == true
+        {
+            boostParticle.Stop(); // Play the bostParticle effect
+            speed = 500; // Set player speed back to normal
+        }
+    }
+    
+    // If Player collides with powerup, activate powerup
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Powerup"))
+        {
+            Destroy(other.gameObject);
+            hasPowerup = true;
+            powerupIndicator.SetActive(true);
+
+            StartCoroutine(PowerupCooldown()); // Call the Powerup Cooldown Coroutine
+        }
+    }
+
+    // Coroutine to count down powerup duration
+    IEnumerator PowerupCooldown()
+    {
+        yield return new WaitForSeconds(powerUpDuration);
+        hasPowerup = false;
+        powerupIndicator.SetActive(false);
+    }
+
+    // If Player collides with enemy
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Rigidbody enemyRigidbody = other.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer =  other.gameObject.transform.position - transform.position;  // Calculate direction enemy should move away from player on collision
+           
+            if (hasPowerup) // if have powerup hit enemy with powerup force
+            {
+                enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+            }
+            else // if no powerup, hit enemy with normal strength 
+            {
+                enemyRigidbody.AddForce(awayFromPlayer * normalStrength, ForceMode.Impulse);
+            }
+
+
+        }
+    }
+
+
+
+}
