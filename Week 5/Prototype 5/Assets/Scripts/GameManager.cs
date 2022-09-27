f35cfdbf3a859e80a95061c88328edaa1ea3abe3
astronaut-nemo <astronautnemo@gmail.com>
2022-09-27 09:10:43 +0300

    Week 5: Lesson 5.2
    Completed lesson 5.2

--- Week 5/Prototype 5/Assets/Scripts/GameManager.cs
+++ Week 5/Prototype 5/Assets/Scripts/GameManager.cs
@@ -1,17 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     // Variables
     public List<GameObject>targets; // Holds the target objects to be spawned
-    public float spawnRate = 1.0f; // Time for spawning targets i.e. coroutine delay
+    public float spawnRate = 2.0f; // Time for spawning targets i.e. coroutine delay
+    private int score; // Holds the score of the player
+    public TextMeshProUGUI scoreText; // Holds the score GUI element
     
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnTarget()); // Calls the spawning coroutine
+
+        // Score keeping
+        score = 0; // Initialising the score
+        UpdateScore(0); // Initialising the score
+        
     }
 
     // Update is called once per frame
@@ -20,6 +28,7 @@ public class GameManager : MonoBehaviour
         
     }
 
+    // Spawning targets
     IEnumerator SpawnTarget()
     {
         while(true)
@@ -28,6 +37,14 @@ public class GameManager : MonoBehaviour
             int index = Random.Range(0, targets.Count); // Randomly select a target from the list
 
             Instantiate(targets[index]); // Spawn randomly selected target
+
         }
+    }
+
+    // Keeping score
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd; // Add to the score
+        scoreText.text = "Score: " + score; // Displays the score
     }
 }
--- Week 5/Prototype 5/Assets/Scripts/Target.cs
+++ Week 5/Prototype 5/Assets/Scripts/Target.cs
@@ -9,15 +9,19 @@ public class Target : MonoBehaviour
     private float maxSpeed = 16; // Maximum speed of upward force
     private float maxTorque = 10; // Range for the torque 
     private float xRange = 4; // Range for the x-position
-    private float ySpawnPos = -2; // POsition to spawn along y-axis (below the background)
+    private float ySpawnPos = -2; // Position to spawn along y-axis (below the background)
+    public int pointValue; // Holds the value of each target
+    public ParticleSystem explosionParticle; // Holds the explosion FX
 
     // References
-    private Rigidbody targetRb; // Holds reference to the object's RIgidbody component
+    private Rigidbody targetRb; // Holds reference to the object's Rigidbody component
+    private GameManager gameManager; // Holds reference to the "Game Manager" object
     
     // Start is called before the first frame update
     void Start()
     {
         targetRb = GetComponent<Rigidbody>(); // Assigns the object's Rigidbody to the variable targetRb
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>(); // Find the Game Manger object in the Hierarchy and get its GameManager.cs script, THEN assign it to the variable gameManager
 
 
         // Apply a random instantaneous force to the Rigidbody
@@ -60,6 +64,8 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation); // Instantiate the explosion particle at the position of the target with the rotation of the particle
+        gameManager.UpdateScore(pointValue); // Change score by amount pointValue everytime an target is clicked on
     }
 
     // Destroy object when it falls onto the sensor i.e. below the screen
