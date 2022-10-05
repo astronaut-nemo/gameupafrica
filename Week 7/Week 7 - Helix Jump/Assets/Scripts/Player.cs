fc31c400a7a9be7d8cd83c287a4c8ee04cd5f1ba
Joanna Oluoch <111391937+astronaut-nemo@users.noreply.github.com>
2022-10-05 14:41:36 +0300

    Week 7: Lesson
    Reached episode 4 of tutorial

---
+++ Week 7/Week 7 - Helix Jump/Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Variables
+    public float bounceForce = 6; // Holds force to bounce player object
+
+    // References
+    public Rigidbody playerRb; // Holds reference to player Rigidbody component
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        playerRb.velocity = new Vector3(playerRb.velocity.x, bounceForce, playerRb.velocity.z); // Give the player a force to bounce in y-direction
+        
+    }
+}
---
+++ Week 7/Week 7 - Helix Jump/Assets/Scripts/Rotator.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    // Variables
+    public float rotationSpeed = 150; // Speed of rotation of the tower
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; // Hide the cursor
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0)) // 0 = left mouse button index
+        {
+            float mouseX = Input.GetAxisRaw("Mouse X"); // Get the input from the mouse in the x-axis without filtering
+            float yRotation = mouseX * rotationSpeed * Time.deltaTime; // Get the speed/frce of rotation about y-axis
+            
+            transform.Rotate(0, -yRotation, 0); // Rotate object about y-axis
+        }
+    }
+}
