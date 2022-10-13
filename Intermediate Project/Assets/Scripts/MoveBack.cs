f0ab93bb4700ad9535a6c9755c8e72506bdf1985
Joanna Oluoch <111391937+astronaut-nemo@users.noreply.github.com>
2022-10-13 14:14:40 +0300

    Main Menu and MoveBack Script
    -Created a Main Menu scene
    -In MoveBack.cs added movement to objects

--- Intermediate Project/Assets/Scripts/MoveBack.cs
+++ Intermediate Project/Assets/Scripts/MoveBack.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class MoveBack : MonoBehaviour
 {
+    // Variables
+    public float speed = 5.0f; // Holds the speed of the object
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,12 @@ public class MoveBack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.back * Time.deltaTime * speed); // Move the object along the z-axis
+    }
+
+    // Destroy objects once they move out of range
+    private void DestroyOutOfBounds()
+    {
+
     }
 }
