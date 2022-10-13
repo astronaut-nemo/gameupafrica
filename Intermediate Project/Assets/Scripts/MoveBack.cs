6545734a8b7d9b2b46239bdab5778e49e41c94f3
Joanna Oluoch <111391937+astronaut-nemo@users.noreply.github.com>
2022-10-13 14:35:29 +0300

    DestroyOutOfBounds and Game Manager
    -Created Game Manager and assigned it a script
    -Created a function in MoveBack.cs to destroy objects out of bounds

--- Intermediate Project/Assets/Scripts/MoveBack.cs
+++ Intermediate Project/Assets/Scripts/MoveBack.cs
@@ -6,6 +6,8 @@ public class MoveBack : MonoBehaviour
 {
     // Variables
     public float speed = 5.0f; // Holds the speed of the object
+    public float outofBound = 10.0f; // Holds the limit at which objects are destroyed off screen
+
 
     // Start is called before the first frame update
     void Start()
@@ -17,11 +19,19 @@ public class MoveBack : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector3.back * Time.deltaTime * speed); // Move the object along the z-axis
+        DestroyOutOfBounds();
     }
 
-    // Destroy objects once they move out of range
+    
+    // Destroy objects tagged Ground, Treat or Obstacle once they move out of range
     private void DestroyOutOfBounds()
     {
-
+        if(transform.position.z < -outofBound)
+        {
+            if(gameObject.CompareTag("Ground") || gameObject.CompareTag("Treat") || gameObject.CompareTag("Obstacle"))
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 }
