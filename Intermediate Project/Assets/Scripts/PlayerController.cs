f4e30d1c4cb05277a1a69f68ed60a8de8cf89b41
Joanna Oluoch <111391937+astronaut-nemo@users.noreply.github.com>
2022-10-13 18:05:23 +0300

    Player Scripting
    -Constrained Player rotation
    -Set jumping conditions to prevent player from jumping infinitely
    -Gave ground pieces a collider to activate isOnGround

--- Intermediate Project/Assets/Scripts/PlayerController.cs
+++ Intermediate Project/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Variables
-    private int jumpForce = 10; // Holds multiplier for the jumping force on Player
+    public float jumpForce = 12; // Holds multiplier for the jumping force on Player
     private float gravityMultiplier = 2; // Multiplier to vary gravity
+
+    public bool isOnGround = true;
 
     // References
     private Rigidbody playerRb; // Holds reference to the player's Rigidbody component
@@ -21,10 +23,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // If the up Key is pressed, make the Player jump by applying a force to its Rb
-        if(Input.GetKeyDown(KeyCode.UpArrow))
+        // If the up Key is pressed and the player is on the ground, make the Player jump by applying a force to its Rb
+        if(Input.GetKeyDown(KeyCode.UpArrow) && isOnGround)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    // Collisions Checker
+    private void OnCollisionEnter(Collision collision)
+    {
+        // If Player is colliding with the ground, set bool to true
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
         }
     }
 }
